e20bc6ba870ea9e473b1b485cf197a60ba81b24e
Hiago Prata <hcprata17@gmail.com>
2018-08-10 10:02:45 -0300

    Criando script de backup

---
+++ backup.sh
@@ -0,0 +1,12 @@
+function backup(){
+	if [ -d "$1" ]; then
+		echo "Criando $1.backup .";
+		cp -rp $1 "$1.backup";
+	elif [ -f "$1" ]; then
+		echo "$1 é um arquivo.";
+	else 
+		echo "$1 não existe.";
+	fi
+}
+
+backup $1
